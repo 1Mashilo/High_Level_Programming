dab7fb9f0f657312c1d048b4bdce115c088268c4
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-10-09 12:57:55 +0200

    Update 6-print_matrix_integer.py

--- 0x03-Python-Data_Structures_Lists_Tuples/6-print_matrix_integer.py
+++ 0x03-Python-Data_Structures_Lists_Tuples/6-print_matrix_integer.py
@@ -4,13 +4,6 @@ def print_matrix_integer(matrix=[[]]):
 
     Args:
         matrix (list of lists): The matrix to be printed, where each inner list represents a row.
-
-    Example:
-        >>> matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-        >>> print_matrix_integer(matrix)
-        1 2 3
-        4 5 6
-        7 8 9
     """
     for row in matrix:
         for col in row:
