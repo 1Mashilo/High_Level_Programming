3b046e9e956cf133c4fa19d833dac582a3d941a8
1Mashilo <christophermashilo@gmail.com>
2023-10-25 19:30:50 +0200

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+
+
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+
+    """Return a list of an object's available attributes."""
+    
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
