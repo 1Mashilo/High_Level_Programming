fe19453e60ca9eb3c0068cfd3002b8e378e15405
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-11-29 20:38:54 +0000

    modified:   0x00-python-hello_world/8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
-
-"""
-This Python script demonstrates string manipulation.
-
-It extracts specific portions from a long string and concatenates them to create a new string.
-"""
-
-# Define a long string describing Python's characteristics
-original_str = "Python is an interpreted, interactive, object-oriented programming" \
-               " language that combines remarkable power with very clear syntax"
-
-# Extract three portions from the original string and concatenate them
-# - Characters 40 to 66 (inclusive)
-# - Characters 108 to 111 (inclusive)
-# - First 6 characters
-new_str = original_str[39:67] + original_str[107:112] + original_str[:6]
-
-# Print the resulting string
-print(new_str)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[-22:-17] + str[0:6]
+print(str)
