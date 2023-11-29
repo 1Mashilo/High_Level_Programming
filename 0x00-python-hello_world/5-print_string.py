7110e1e58e91c459efbfafb189c6597a67171eb6
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-11-29 20:23:08 +0000

    modified:   0x00-python-hello_world/5-print_string.py

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 """
-This Python script manipulates a string by repeating it and printing a substring.
+This Python script manipulates a string by
+repeating it and printing a substring.
 
-It assigns the string "Holberton School" to the variable 'str', then it prints 'str' repeated three times with spaces in between, and also prints the first 9 characters of 'str'.
+It assigns the string "Holberton School"
+to the variable 'str', then it prints 'str'
+repeated three times with spaces in between,
+and also prints the first 9 characters of 'str'.
 """
 
 str = "Holberton School"
 
-print(" ".join([str] * 3))
+print(3 * str)
 print(str[:9])
