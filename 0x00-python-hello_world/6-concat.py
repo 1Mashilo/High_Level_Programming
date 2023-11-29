de83a7b1c480449e1e1494936f9d9ea7b6172640
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-11-29 20:33:33 +0000

    ;l

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-
-"""
-This Python script concatenates two strings and prints a welcome message.
-
-It assigns the strings "Holberton" and "School" to 'str1' and 'str2', respectively. 
-Then, it concatenates 'str1' and 'str2' with a space in between and prints a welcome message using string formatting.
-"""
-
 str1 = "Holberton"
 str2 = "School"
-
-str1 += " " + str2
-print("Welcome to {}!".format(str1))
+str1 = str1 + " " + str2
+print(f"Welcome to {str1}!")
