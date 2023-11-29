c86d04d5a6a55d62047343bef096f2906e397e50
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-11-29 01:58:31 +0000

    modified 0x01-python-if_else_loops_functions/3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,20 +1,4 @@
 #!/usr/bin/python3
-"""
-Prints the lowercase alphabet, excluding 'q' and 'e', in a single
-line followed by a new line.
-
-This program uses a for loop to iterate through
-the ASCII values of lowercase letters (from 'a' to 'z').
-Within the loop, it checks if the current
-letter is not 'q' and not 'e' using an if condition.
-If the condition is met, it prints the letter using the 'print' function
-with the 'end' argument set to an empty string to avoid line breaks.
-
-The result is the lowercase alphabet printed in a single line,
-excluding 'q' and 'e', followed by a new line.
-
-"""
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
-print()
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print('{:c}'.format(i), end='')
