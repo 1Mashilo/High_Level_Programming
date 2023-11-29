e2f333bad0582eb88e1d2fc63c848a098765ce43
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-11-29 02:02:32 +0000

    modified:   0x01-python-if_else_loops_functions/5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -8,9 +8,6 @@ with two digits. Numbers are separated by a comma and space except
 for the last number, which is followed by a new line.
 
 """
-
-for number in range(100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
+for num in range(0, 99):
+    print('{:02d}, '.format(num), end='')
+print('99')
