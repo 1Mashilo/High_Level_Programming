e7fc6a771e67412508347a6342ed5b67b04e7d61
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-11-29 01:52:08 +0000

    modified:   0x01-python-if_else_loops_functions/2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """
-Prints lowercase letters from 'a' to 'z' in a single line.
+Prints the ASCII alphabet in lowercase on the same line.
 
-This code snippet uses a for loop to iterate through the
-range of Unicode code points
-corresponding to lowercase letters ('a' to 'z'). It prints
-each letter sequentially in a single line.
+This code snippet uses a for loop to iterate through the range of
+Unicode code points corresponding to lowercase letters ('a' to 'z').
+It prints each letter sequentially on the same line without line breaks.
 
-The 'chr()' function is used to convert the code points into
-characters, and the 'end=""' argument
-in the 'print()' function ensures that the letters are
-printed on the same line without line breaks.
+The 'chr()' function is used to convert the code points into characters.
 
 """
 for i in range(ord('a'), ord('z') + 1):
