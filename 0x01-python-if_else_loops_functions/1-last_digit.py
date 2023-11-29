33ddf73f0b806320cf5fdecc0d25ab1043125339
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-11-29 01:40:01 +0000

    modified:   0x01-python-if_else_loops_functions/1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
-"""
-his Python program assigns a random signed number to the variable 'number'
-each time it is executed
-and prints its last digit along with specific conditions.
-
-It prints the string 'Last digit of [number] is [last_digit]' followed by:
-- If the number is greater than 5: 'and is greater than 5'
-- If the number is 0: 'and is 0'
-- If the number is less than 6 and not 0: 'and is less than 6 and not 0'
-Followed by a new line.
-"""
-
 import random
-
-number = random.randint(-10, 20)
-
-last_digit = abs(number) % 10
-
-if number > 5:
-    message = "and is greater than 5"
-elif number == 0:
-    message = "and is 0"
+number = random.randint(-10000, 10000)
+if number < 0:
+    lastdigit = number % -10
 else:
-    message = "and is less than 6 and not 0"
-
-print(f"Last digit of {number} is {last_digit} {message}")
+    lastdigit = number % 10
+if lastdigit > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, lastdigit))
+elif lastdigit < 6 and lastdigit != 0:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, lastdigit))
+else:
+    print("Last digit of {:d} is 0 and is 0".format(number))
