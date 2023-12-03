4885a28a6c7bb5c16ce941f4790d343288d85af8
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-03 18:17:46 +0000

    Changing file modifications for task 2 file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,22 +1,18 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
-    """
-    Print information about the command-line arguments.
-
-    This script calculates and displays the number of command-line arguments
-    provided when running the script, along with a list of the arguments and
-    their positions.
-    """
     import sys
+    i = len(sys.argv) - 1
 
-    num_args = len(sys.argv) - 1
-
-    if num_args == 0:
-        print("No arguments provided.")
-    elif num_args == 1:
-        print("One argument provided:")
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print(f"{num_args} arguments provided:")
+        print("{} arguments:".format(i))
 
-    for i in range(num_args):
-        print(f"Argument {i + 1}: {sys.argv[i + 1]}")
-
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
