290a917f564dd3f58f4b52d6ebe67ebce10dba12
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-03 18:30:53 +0000

    Making file for task 3 executable

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,9 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
-    """
-    Calculate and print the sum of command-line arguments.
-
-    This script calculates the sum of all command-line arguments provided when
-    running the script and prints the result.
-    """
     import sys
-
-    total = 0
-
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-
-    print(f"Sum of provided arguments: {total}")
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
+    
