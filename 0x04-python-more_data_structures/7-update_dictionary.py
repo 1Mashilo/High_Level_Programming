c73c88c452c6192839833582623dd8bed45468c7
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-06 22:02:48 +0200

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-
-def update_dictionary(dictionary, key, value):
-    """
-    Replace or add a key/value pair in a dictionary.
-
-    Args:
-        dictionary (dict): The dictionary to be updated.
-        key (str): The key to be added or updated.
-        value: The value to be associated with the key.
-
-    Returns:
-        dict: The updated dictionary.
-    """
-    dictionary[key] = value
-    return dictionary
-
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return (a_dictionary)
