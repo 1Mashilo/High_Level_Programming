7793dd816f28c19b6daaf0cb0bd865b753d6d633
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-06 21:55:54 +0200

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,42 +1,9 @@
-def multiply_values_by_2(input_dict):
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dir = a_dictionary.copy()
+    list_keys = list(new_dir.keys())
 
-    """
-    Create a new dictionary with values doubled from the input dictionary.
+    for i in list_keys:
+        new_dir[i] *= 2
 
-    Args:
-        input_dict (dict): The dictionary from which values will be doubled.
-
-    Returns:
-        dict: A new dictionary with the same keys and values doubled.
-    """
-    # Create a copy of the input dictionary
-    doubled_dict = input_dict.copy()
-
-    # Get a list of keys from the copied dictionary
-    keys_list = list(doubled_dict.keys())
-
-    # Iterate through keys and double values
-    for key in keys_list:
-        doubled_dict[key] *= 2
-
-    return doubled_dict
-
-# Test case 
-
-# Define a sample dictionary with integer values
-sample_dict = {
-    'a': 5,
-    'b': 10,
-    'c': 15,
-    'd': 20
-}
-
-# Call the function to create a new dictionary with doubled values
-doubled_dict = multiply_values_by_2(sample_dict)
-
-# Print the original and modified dictionaries
-print("Original Dictionary:")
-print(sample_dict)
-
-print("\nModified Dictionary:")
-print(doubled_dict)
+    return (new_dir)
