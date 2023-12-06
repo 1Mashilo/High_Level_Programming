1eed729be2737a99418f8679712c6d315fb78847
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-06 21:59:43 +0200

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-
-def delete_key_from_dictionarya(dictionary, key):
-    """
-    Delete a key from a dictionary, if it exists.
-
-    Args:
-        dictionary (dict): The dictionary from which to delete the key.
-        key (str): The key to be deleted.
-
-    Returns:
-        dict: The updated dictionary after key deletion.
-    """
-    if key in dictionary:
-        del dictionary[key]
-   return dictionary
+def simple_delete(a_dictionary, key=""):
+    if a_dictionary.get(key) is not None:
+        del a_dictionary[key]
+    return (a_dictionary)
