f138d2ef1a1355b0851c2565ef8712b2d886491a
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-07 21:26:25 +0200

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,29 +1,9 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_list = set(my_list)
+    num = 0
 
-def sum_unique_integers(input_list=[]):
-    """
-    Calculate the sum of unique integers in the input list.
+    for i in uniq_list:
+        num += i
 
-    This function takes a list of integers and returns the sum of unique integers.
-    Each unique integer is included only once in the summation, even if it appears
-    multiple times in the input list.
-
-    Args:
-        input_list (list): A list of integers to find the sum of unique elements.
-
-    Returns:
-        int: The sum of unique integers in the input list.
-
-    """
-    unique_integers = set()
-
-    for integer in input_list:
-        if integer not in unique_integers:
-            unique_integers.add(integer)
-
-    sum_of_unique = 0
-
-    for unique_integer in unique_integers:
-        sum_of_unique += unique_integer
-
-    return sum_of_unique
+    return (num)
