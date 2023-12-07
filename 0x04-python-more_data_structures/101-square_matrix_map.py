4947d9014f7de3e39bb91de4a6f4c19e45cb9ff9
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-07 21:44:15 +0200

    Update 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def square_matrix_map(matrix=[]):
     """
     Calculate the square of each element in a matrix using `map`.
@@ -9,4 +11,5 @@ def square_matrix_map(matrix=[]):
         list: A new matrix of the same size as the input matrix, where each element is the square of the corresponding
               element in the input matrix.
     """
-    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
+
+    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix))) 
