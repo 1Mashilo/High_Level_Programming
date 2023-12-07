848d0733b7fb90ed1553f049ef1fef41db78e36a
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-07 21:28:05 +0200

    Update 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
+def number_keys(a_dictionary):
+    num = 0
+    list_keys = list(a_dictionary.keys())
 
-def count_keys_in_dictionary(input_dict):
-    """
-    Count and return the number of keys in a dictionary.
+    for i in list_keys:
+        num += 1
 
-    Args:
-        input_dict (dict): The dictionary for which to count keys.
-
-    Returns:
-        int: The number of keys in the dictionary.
-    """
-    key_count = 0
-    keys_list = list(input_dict.keys())
-
-    for key in keys_list:
-        key_count += 1
-
-    return key_count
+    return (num) 
