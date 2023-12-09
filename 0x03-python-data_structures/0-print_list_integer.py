83215c4523ccbd40624de8614dafb4225ff4d6a8
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-09 12:31:36 +0000

    docs: Add docstring to print_list_integer function
    This commit adds a docstring to the print_list_integer function, providing
    information about its purpose, input parameters, and return value. The
    docstring follows PEP 8 conventions.

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
+    """
+    Print each integer in the given list.
+
+    Args:
+        my_list (list): A list of integers.
+
+    Returns:
+        None
+    """
     for i in range(len(my_list)):
-        print("{:d}".format(my_list[i])) 
-        
+        print("{:d}".format(my_list[i]))
