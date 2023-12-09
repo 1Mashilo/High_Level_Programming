69e60814ca952a95b092a0015d38b97cf92caed0
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-09 12:43:49 +0000

    docs: Add docstring to script demonstrating print_reversed_list_integer function
    This commit adds a docstring to the script, providing information about its
    purpose and usage. The docstring follows PEP 8 conventions.

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+"""
+Script to demonstrate the print_reversed_list_integer function.
+
+Usage:
+    Run this script to import the print_reversed_list_integer function
+    from the 3-print_reversed_list_integer module and call it with a predefined list.
+"""
+
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
 my_list = [1, 2, 3]
 print_reversed_list_integer(my_list)
-
