7d81d303342ad6c782a0a4dca3a0ecb8ef596744
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-09 12:54:19 +0000

    fixed PEP8

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
@@ -8,12 +7,12 @@ def print_reversed_list_integer(my_list=[]):
     Args:
         my_list (list of int): The list of integers to be printed in reverse order.
     """
-    
+
     if not isinstance(my_list, list):
         print("Input is not a list.")
         return
 
     reversed_list = my_list[::-1]
-    
+
     for i in reversed_list:
         print("{:d}".format(i))
