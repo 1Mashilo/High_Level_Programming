4d717de89272c885877a097ad6b63bc16466865e
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-18 20:28:45 +0200

    Submit solution

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-def safe_function(fct, *args):
-    """
-    Execute a function safely.
-
-    Args:
-        fct: The function to execute.
-        args: Arguments for fct.
-
-    Returns:
-        Any: The result of the function execution, or None if an error occurs.
-    """
-    try:
-        result = fct(*args)
-        return result
-    except Exception as e:
-        print(f"Exception: {e}", file=sys.stderr)
-        return None
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if (i > a):
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / i
+        except:
+            result = b + a
+            break
+    return (result) 
