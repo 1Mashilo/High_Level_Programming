f7cd97475ad90d16ab9230048201bc951df5b705
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-19 22:31:42 +0100

    submit solution

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,39 +1,16 @@
 #!/usr/bin/python3
+"""class Square that defines a square"""
 
-class Square:
-    """A class that represents a square.
 
-    This class allows the creation of square objects with a specified size,
-    and it enforces that the size is a non-negative integer. Additionally, it
-    provides a method to calculate the area of the square.
-
-    Attributes:
-        __size (int): The size of the square.
-
-    Methods:
-        area(): Calculate and return the area of the square.
-    """
+class Square():
+    """square class with it's size and proper validation"""
 
     def __init__(self, size=0):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square. It must be a non-negative integer.
-        Raises:
-            TypeError: If the size is not an integer.
-            ValueError: If the size is a negative integer.
-        """
-        if not isinstance(size, int):
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif (size < 0):
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        
-        """Calculate and return the current area of the square.
-
-        Returns:
-            int: The area of the square (size * size).
-        """
-        return (self.__size * self.__size)
+        return self.__size ** 2
