61c14b64446a41277b0b9e68f465dbb89fb13384
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2023-12-19 22:30:13 +0100

    submit solution

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
+"""class Square that defines a square"""
 
-class Square:
-    """A class for representing squares.
 
-    This class allows the creation of square objects with a specified size. The size
-    should be a non-negative integer. The size is stored as a private attribute, and
-    validation checks ensure the size is valid during object creation.
-
-    Attributes:
-        __size (int): The size of the square.
-    """
+class Square():
+    """square class with it's size and proper validation"""
 
     def __init__(self, size=0):
-
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square. It should be a non-negative integer.
-        """
-        if not isinstance(size, int):
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif (size < 0):
             raise ValueError("size must be >= 0")
-
         self.__size = size
+        
