537eaae24bf01b865c227a3eac2b3b7aeb2cf784
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2024-01-10 19:28:55 +0200

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""Defines a text-indentation function."""
+
 
 def text_indentation(text):
-    """
-    Print text with two new lines after each '.', '?', and ':'.
+    """Print text with two new lines after each '.', '?', and ':'.
 
     Args:
         text (string): The text to print.
@@ -12,17 +13,17 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    current_index = 0
-    while current_index < len(text) and text[current_index] == ' ':
-        current_index += 1
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
-    while current_index < len(text):
-        print(text[current_index], end="")
-        if text[current_index] == "\n" or text[current_index] in ".?:":
-            if text[current_index] in ".?:":
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
                 print("\n")
-            current_index += 1
-            while current_index < len(text) and text[current_index] == ' ':
-                current_index += 1
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
             continue
-        current_index += 1
+        c += 1
