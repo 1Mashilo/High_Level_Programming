2a8901a130bff959490305faf88dc0f4d0ac8ff9
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2024-01-10 19:44:38 +0200

    submit solution

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python3
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
@@ -26,3 +26,4 @@ class Rectangle(BaseGeometry):
         string = "[" + str(self.__class__.__name__) + "] "
         string += str(self.__width) + "/" + str(self.__height)
         return string
+        
