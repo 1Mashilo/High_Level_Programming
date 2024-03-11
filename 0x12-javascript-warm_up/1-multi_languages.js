1003a557a46c06789120721a7c7f96411a8027a0
1Mashilo <christophermashilo@gmail.com>
2024-03-11 06:13:12 -0700

    Fix linter issues: remove trailing spaces and extra blank lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node 
+#!/usr/bin/node
 
 const language1 = 'C is fun';
 const language2 = 'Python is cool';
@@ -7,4 +7,3 @@ const language3 = 'JavaScript is amazing';
 console.log(language1);
 console.log(language2);
 console.log(language3);
-
