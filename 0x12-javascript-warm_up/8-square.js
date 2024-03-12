1b544f94fbb2baa8c08d8c720a624a3656afa0d5
1Mashilo <christophermashilo@gmail.com>
2024-03-12 07:44:41 -0700

    submit task

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const size = parseInt(arg);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let row = 0; row < size; row++) {
+    let line = '';
+
+    for (let col = 0; col < size; col++) {
+      line += 'X';
+    }
+
+    console.log(line);
+  }
+}
