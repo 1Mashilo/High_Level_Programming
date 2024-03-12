6c6f79d4689f8ae19dc6a21a05dfff8fc6300e44
1Mashilo <christophermashilo@gmail.com>
2024-03-12 06:19:35 -0700

    initial commit

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+const converted = parseInt(arg); 
+
+if (isNaN(converted)) {
+    console.log("Not a number");
+} else {
+    console.log("My Number:", converted);
+}
+
