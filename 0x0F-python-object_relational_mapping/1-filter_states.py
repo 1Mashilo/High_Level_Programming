c7bcf10757dae4ff92d221089f7c33e631897ce1
1Mashilo <christophermashilo@gmail.com>
2024-03-16 10:44:28 -0700

    Enforced PEP8 compliance using pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Lists all states with names starting with N from the database hbtn_0e_0_usa.""" 
+"""Lists all states with names starting with N from
+the database hbtn_0e_0_usa."""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
@@ -20,7 +20,9 @@ if __name__ == "__main__":
         cursor = db.cursor()
 
         # Execute SQL query (with filtering)
-        cursor.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        cursor.execute("SELECT id, "
+                       "name FROM states WHERE name "
+                       "LIKE 'N%' ORDER BY id ASC")
         results = cursor.fetchall()
 
         # Print Results
@@ -36,4 +38,3 @@ if __name__ == "__main__":
             cursor.close()
         if db:
             db.close()
-
