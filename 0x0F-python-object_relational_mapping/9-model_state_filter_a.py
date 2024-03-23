da49ae93cee99da7f454a70ae92f3bae5facbb33
1Mashilo <christophermashilo@gmail.com>
2024-03-23 09:28:45 -0700

    fixed checker issue

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
+"""
+Script to list all State objects containing the letter 'a' from the database hbtn_0e_6_usa.
 """
 import sys
 from model_state import Base, State
@@ -13,8 +14,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name.like('%a%'))
-    if instance is None:
-        print("Nothing")
-    else:
+    for instance in session.query(State).filter(State.name.like('%a%')):
         print(instance.id, instance.name, sep=": ")
