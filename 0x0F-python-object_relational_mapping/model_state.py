5fad3749b7c243df908b7828b03e93be242dc94d
1Mashilo <christophermashilo@gmail.com>
2024-03-23 08:19:07 +0200

    improved documentation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,23 @@
 #!usr/bin/python3
 
-from sqlalchemy import Column, Integer, String
+"""
+This module provides the SQLAlchemy model definition for representing states within
+a database. It includes the 'State' class.
+"""
+
+from sqlalchemy import Column, Integer, String, Identity
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class State(Base):
+    """
+    A class to represent states within a database.
 
+    Attributes:
+        id (int): The unique identifier for the state.
+        name (str): The name of the state.
+    """
     __tablename__ = "state"
-
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, Identity(always=True), primary_key=True)
     name = Column(String(128), nullable=False)
