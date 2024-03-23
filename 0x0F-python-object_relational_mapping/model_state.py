69910526cc150969c3e5e6653b9fcad4ad48fa5e
1Mashilo <christophermashilo@gmail.com>
2024-03-23 08:46:03 +0200

    Enable metadata customization for SQLAlchemy models

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,8 +8,8 @@ a database. It includes the 'State' class.
 from sqlalchemy import Column, Integer, String, Identity
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 class State(Base):
     """
     A class to represent states within a database.
@@ -19,5 +19,5 @@ class State(Base):
         name (str): The name of the state.
     """
     __tablename__ = "state"
-    id = Column(Integer, Identity(always=True), primary_key=True)
+    id = Column(Integer,unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
