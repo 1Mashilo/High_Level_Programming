6d49f4f09a4646a85b3df208d0f51f878ebf57be
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2024-04-01 16:15:54 +0000

    initial commit

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Sends a GET request to the specified URL and returns the value of the 'X-Request-Id' header.
+
+    Args:
+        url (str): The URL to send the GET request to.
+
+    Returns:
+        str: The value of import requests
+
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
