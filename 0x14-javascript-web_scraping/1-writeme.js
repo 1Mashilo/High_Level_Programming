2ff443efdc714544cfb7df678799be361786d800
1Mashilo <christophermashilo@gmail.com>
2024-04-25 03:32:12 -0700

    semistandard validation

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
 
-
 const fs = require('fs');
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
-
-
-if (!filePath || !contentToWrite) {
-  console.error('Please provide both a file path and content to write as arguments.');
-  process.exit(1);
-}
 
 fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
   if (err) {
